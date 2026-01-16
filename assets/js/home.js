0f733d4af4c4f66f7ae59aacf3e6b652df49c7d3
Shreetheja <shreeteja.vasala@gmail.com>
2026-01-16 19:54:05 +0530

    Login popup attached

---
+++ assets/js/home.js
@@ -0,0 +1,6 @@
+function openLogin() {
+    document.getElementById("loginPopUp").style.display = "flex";
+}
+function closeLogin() {
+    document.getElementById("loginPopUp").style.display = "none";
+}
